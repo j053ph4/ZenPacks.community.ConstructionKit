938224f43ff18080910f3249c8116d929a1f5623
Joseph Anderson <janderson@agero.com>
2015-03-31 08:46:29 -0500

    changed license to Apache

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.ConstructionKit"
-VERSION = "3.0"
+VERSION = "3.1"
 AUTHOR = "Joseph Anderson"
 LICENSE = "Apache"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
